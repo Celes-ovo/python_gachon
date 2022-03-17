b254b29aaf558538f8aaf6788ba2c90f77039465
Celes-ovo <74448836+Celes-ovo@users.noreply.github.com>
2022-03-17 18:32:43 +0900

    upload

---
+++ ch2/lab02_02.py
@@ -0,0 +1,11 @@
+print("정보 입력")
+
+person_name = input("받는 사람 : ")
+person_address = input("주소 : ")
+weight = int(input("무게(g) : "))
+
+print("=========================")
+
+print("받는 사람 : ", person_name)
+print("주소 : ", person_address)
+print("무게 : ", weight)
---
+++ ch2/lab02_03.py
@@ -0,0 +1,21 @@
+# 숫자를 입력하는 계산기 만들기
+
+num1 = int(input("1번째 숫자 입력 : "))
+num2 = int(input("2번째 숫자 입력 : "))
+
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+result5 = num1 % num2
+result6 = num1 ** num2
+
+
+print("num1 + num2 = ", result1)
+print("num1 - num2 = ", result2)
+print("num1 * num2 = ", result3)
+print("num1 / num2 = ", result4)
+print("num1 % num2 = ", result5)
+print("num1 ** num2 = ", result6)
+
+print("[Log] Done")
