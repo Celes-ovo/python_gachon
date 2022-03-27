0785f0ea6f8f9de40fecdbbfbeec4d4afd7c837e
Celes-ovo <74448836+Celes-ovo@users.noreply.github.com>
2022-03-28 02:27:45 +0900

    up

---
+++ ch4/practice1.py
@@ -0,0 +1,8 @@
+print(type(123))
+print(type('123'))
+print(type('hello'))
+print(type(12.2))
+print(type([1, 2, 3, 4]))
+print(type((1, 2, 3, 4)))
+print(type({1, 2, 3, 4}))
+print(type({'a':10, 'b':20, 'c':30}))
---
+++ ch4/practice2.py
@@ -0,0 +1,8 @@
+string = '안녕'
+string2 = string * 3
+
+print(string2)
+
+print(len(string2))
+print(string2.count('안녕'))
+print(string2.find('안녕'))
